79bc2e43c3bf868383a06607d28bf65485b2d536
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-08-21 22:24:22 +0530

    Updated Solution - GfG to GitHub

--- Easy/Surround the 1's/surround-the-1s.cpp
+++ Easy/Surround the 1's/surround-the-1s.cpp
@@ -9,37 +9,32 @@ class Solution {
 public:
     int Count(vector<vector<int> >& matrix) {
         // Code here
-       
-        int result = 0;
+        int soln = 0;
         
         vector<int> row = {-1, -1, 0, 1, 1, 1, 0, -1};
         vector<int> col = {0, 1, 1, 1, 0, -1, -1, -1};
-        
         int n = matrix.size();
         int m = matrix[0].size();
         
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                int count = 0;
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                int getcount = 0;
                 if (matrix[i][j] == 1) {
                     for (int k = 0; k < 8; k++) {
                         int r = i + row[k];
                         int c = j + col[k];
                         
                         if (r >= 0 && r < n && c >= 0 && c < m && matrix[r][c] == 0) {
-                            count++;
+                            getcount++;
                         }
                     }
-                    if (count > 0 && count % 2 == 0) {
-                        result++;
+                    if (getcount > 0 && getcount % 2 == 0) {
+                        soln++;
                     }
                 }
             }
         }
-        return result;
-    
-
-
+        return soln;
     }
 };
 
