97e474af8311d721b55824242ccf799c1f54e52f
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-09-05 20:54:48 +0530

    Updated Solution - GfG to GitHub

--- Easy/Print adjacency list/print-adjacency-list.cpp
+++ Easy/Print adjacency list/print-adjacency-list.cpp
@@ -8,17 +8,17 @@ class Solution {
     // Function to return the adjacency list for each vertex.
     vector<vector<int>> printGraph(int V, vector<pair<int,int>>edges) {
         // Code here
-         vector<vector<int>> adjencency_List(V);
+         vector<vector<int>> adjcentList(V);
 
         for (int i = 0; i < edges.size(); i++) {
             int u = edges[i].first;
             int v = edges[i].second;
             
-            adjencency_List[u].push_back(v);
-            adjencency_List[v].push_back(u);
+            adjcentList[u].push_back(v);
+            adjcentList[v].push_back(u);
         }
 
-        return adjencency_List;
+        return adjcentList;
     }
 };
 
