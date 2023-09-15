df86ddd4a2dae118fb6958a95810cb285b6cf1be
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-09-15 14:54:33 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Partition Equal Subset Sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int equalPartition(int N, int arr[])
+    {
+        // code here
+         int total = 0;
+        for(int i=0;i<N;i++)
+        total+=arr[i];
+        if(total%2!=0)
+        return 0;
+        vector<int>dp(total+1,0);
+        dp[0]=1;
+        for(int i=1;i<=N;i++)
+        {
+            for(int j=total/2;j>=arr[i-1];j--)
+            {
+              if(arr[i-1]<=j)
+               {
+                dp[j]=dp[j] || dp[j-arr[i-1]];
+               }
+            }
+        }
+        return dp[total/2];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        int arr[N];
+        for(int i = 0;i < N;i++)
+            cin>>arr[i];
+        
+        Solution ob;
+        if(ob.equalPartition(N, arr))
+            cout<<"YES\n";
+        else
+            cout<<"NO\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
