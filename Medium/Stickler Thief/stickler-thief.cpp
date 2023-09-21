734068f18d29c8e5d229b716316d6552295704f5
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-09-21 20:36:08 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Stickler Thief/stickler-thief.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        // Your code here
+        // int sum1 = 0;
+        // int sum2 = 0;
+        // for(int i = 0; i < n; i = i + 2)
+        // {
+        //     sum1 = sum1 + arr[i];
+        // }
+        // for(int i = 1; i < n; i = i + 2)
+        // {
+        //     sum2 = sum2 + arr[i];
+        // }
+        // if(sum1 > sum2)
+        // {
+        //     return sum1;
+        // }
+        // return sum2;
+        
+         if (n == 0) {
+        return 0;
+    }
+
+    if (n == 1) {
+        return arr[0]; 
+    }
+
+    vector<int> dp(n, 0); 
+
+    dp[0] = arr[0]; 
+    dp[1] = max(arr[0], arr[1]); 
+
+    for (int i = 2; i < n; i++) {
+        // The maximum money at the i-th house is the maximum of two options:
+        // 1. Skip the current house and take the maximum money from the previous house.
+        // 2. Loot the current house and add its money to the maximum money from two houses ago.
+        dp[i] = max(dp[i - 1], dp[i - 2] + arr[i]);
+    }
+
+    return dp[n - 1]; // Return the maximum money at the last house.
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
