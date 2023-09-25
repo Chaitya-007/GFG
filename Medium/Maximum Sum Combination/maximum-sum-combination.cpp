2970f03dd7c0b5c2075d9b21cb578c77c9c05e17
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-09-25 20:31:35 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Sum Combination/maximum-sum-combination.cpp
+++ Medium/Maximum Sum Combination/maximum-sum-combination.cpp
@@ -8,69 +8,34 @@ class Solution {
   public:
     vector<int> maxCombinations(int n, int k, vector<int> &a, vector<int> &b) {
         // code here
-        
-        // sort(A.begin(),A.end());
-        // sort(B.begin(),B.end());
-        
-        // int count = 0;
-        
-        // vector<int>v;
-        // vector<int>v2;
-        
-        
-        // for(int i = A.size() - 1; i >= 0; i--)
-        // {
-        //     for(int j = B.size() - 1; j >=0; j--)
-        //     {
-        //         int sum = (A[i] + B[j]);
-        //         v.emplace_back(sum);
-        //     }
-        // }
-        
-        // sort(v.begin(),v.end());
-        
-        // // for(auto itr = v.rbegin(); count != K; itr--)
-        // // {
-        // //     cout<<*itr<<" ";
-        // // }
-        
-        // for(int i = v.size() - 1; count != K ; i--)
-        // {
-        //     v2.emplace_back(v[i]);
-        //     count++;
-        // }
-        
-        // return v2;
-        
-         sort(a.begin(),a.end());
+        sort(a.begin(),a.end());
         sort(b.begin(),b.end());
         vector<int>ans(k,0);
         int cnt=0;
-        map<pair<int,int>,int>mp;
-        priority_queue<pair<int,pair<int,int>>>pq;
-        pq.push({a[n-1]+b[n-1],{n-1,n-1}});
-        mp[{n-1,n-1}]=1;
+        map<pair<int,int>,int>mps;
+        priority_queue<pair<int,pair<int,int>>>pqs;
+        pqs.push({a[n-1]+b[n-1],{n-1,n-1}});
+        mps[{n-1,n-1}]=1;
         
-        while(!pq.empty())
+        while(!pqs.empty())
         {
-            int x=pq.top().second.first;
-            int y=pq.top().second.second;
-            pq.pop();
-            if(x-1>=0&&mp.find({x-1,y})==mp.end())
+            int x=pqs.top().second.first;
+            int y=pqs.top().second.second;
+            pqs.pop();
+            if(x-1>=0&&mps.find({x-1,y})==mps.end())
             {  
-              pq.push({a[x-1]+b[y],{x-1,y}});
-              mp[{x-1,y}]==1;
+              pqs.push({a[x-1]+b[y],{x-1,y}});
+              mps[{x-1,y}]==1;
             }
-            if(y-1>=0&&mp.find({x,y-1})==mp.end())
+            if(y-1>=0&&mps.find({x,y-1})==mps.end())
             {
-                pq.push({a[x]+b[y-1],{x,y-1}});
-                mp[{x,y-1}]==1;
+                pqs.push({a[x]+b[y-1],{x,y-1}});
+                mps[{x,y-1}]==1;
             }
             ans[cnt++]=a[x]+b[y];
             if(cnt==k)return ans;
-        }
     }
-    
+    }
 };
 
 //{ Driver Code Starts.
