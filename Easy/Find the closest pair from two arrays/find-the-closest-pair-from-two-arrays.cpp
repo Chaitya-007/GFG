2c3a493b5b37e089521b276325574d7bfb3dbbf8
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-09-27 18:46:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find the closest pair from two arrays/find-the-closest-pair-from-two-arrays.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function teamplate for C++
+
+class Solution{
+  public:
+    vector<int> printClosest(int arr[], int brr[], int n, int m, int x) {
+        //code here
+         int i=n-1;
+        int j=0;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
+        
+        while(i>=0&&j<m){
+            int sum=arr[i]+brr[j];
+            int diff=abs(sum-x);
+            pq.push({diff,{arr[i],brr[j]}});
+            
+            if(sum<x){
+                j++;
+            }
+            else{
+                i--;
+            }
+        }
+        
+        auto it=pq.top();
+        pq.pop();
+        return{it.second.first,it.second.second};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	int m,n,x;
+	while(t--)
+    {
+        cin>>n>>m;
+        int a[n],b[m];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+            
+        for(int i=0;i<m;i++)
+            cin>>b[i];
+            
+        cin>>x;
+        
+        vector<int> ans;
+        Solution ob;
+        ans = ob.printClosest(a, b, n, m, x);
+        cout << abs(ans[0] + ans[1] - x) << endl;
+        
+    }
+    return 0;
+}
+
+// } Driver Code Ends
