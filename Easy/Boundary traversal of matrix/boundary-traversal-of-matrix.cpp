4686642036217600b751245503d1d4a099626f33
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-10-01 20:49:18 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Boundary traversal of matrix/boundary-traversal-of-matrix.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+
+class Solution
+{   
+    public:
+    //Function to return list of integers that form the boundary 
+    //traversal of the matrix in a clockwise manner.
+    vector<int> boundaryTraversal(vector<vector<int> > matrix, int n, int m) 
+    {
+        // code here
+        vector<int> ans_new;
+        for(int i = 0; i < m; i++)
+            ans_new.push_back(matrix[0][i]);
+        for(int i = 1; i < n - 1; i++)
+            ans_new.push_back(matrix[i][m - 1]);
+        if(n == 1)
+            return ans_new;
+        for(int i = m - 1; i >= 0; i--)
+            ans_new.push_back(matrix[n - 1][i]);
+        if(m == 1)
+            return ans_new;
+        for(int i = n - 2; i >= 1; i--)
+            ans_new.push_back(matrix[i][0]);
+        return ans_new;
+        
+        // vector<int>vec;
+        
+        
+        // for(int j = 0; j < m; j++)
+        // {
+        //     vec.emplace_back(matrix[0][j]);
+        // }
+        
+        // if(n == 1)
+        // {
+        //     return vec;
+        // }
+        
+        // int j = (m-1);
+        // for(int i = 1; i < n ; i++)
+        // {
+        //     vec.emplace_back(matrix[i][j]);
+        // }
+        // int i = (n-1);
+        // for(j = (m-2); j >= 0 ;j--)
+        // {
+        //     vec.emplace_back(matrix[i][j]);
+        // }
+        // j = 0;
+        // for( i = (n-2) ; i>= 1; i--)
+        // {
+        //     vec.emplace_back(matrix[i][j]);
+        // }
+        // return vec;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--) 
+    {
+        int n,m;
+        cin>>n>>m;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(m, 0);
+            for( int j=0; j<m; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.boundaryTraversal(matrix, n, m);
+        for (int i = 0; i < result.size(); ++i)
+                cout<<result[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
