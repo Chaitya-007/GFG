0a6997383ee13f99488ce2b55bc9bb7fc137f8a6
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-11-14 17:15:24 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Check if strings are rotations of each other or not/check-if-strings-are-rotations-of-each-other-or-not.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        string res  = s2;
+        res += s2;
+        
+        if(res.find(s1) != string::npos)
+        {
+            return true;
+        }
+        return false;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        Solution obj;
+        cout<<obj.areRotations(s1,s2)<<endl;
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
