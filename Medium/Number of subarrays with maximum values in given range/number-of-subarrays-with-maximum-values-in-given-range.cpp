e37ea242fbe54805a8cdb3b45011d7ef7ccbf444
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-12-07 20:07:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of subarrays with maximum values in given range/number-of-subarrays-with-maximum-values-in-given-range.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    long countSubarrays(int a[], int n, int L, int R)
+    {
+        // Complete the function
+          long long int ans=0;
+        int start=0;
+        int p=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]>=L && a[i]<=R)
+            {
+                p=i-start+1;
+            }
+            else if(a[i]>R)
+            {
+                start=i+1;
+                p=0;
+            }
+            ans=ans+p;
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+// driver program
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        
+        int n ,l,r;
+        cin >> n>>l>>r;
+        int a[n];
+        for(int i=0;i<n;i++)
+        cin >> a[i];
+        Solution ob;
+    	cout << ob.countSubarrays(a, n, l, r)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
