ef24431efd005fffa7df8a242bbfca529446273c
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-12-09 22:21:39 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Smith Number/smith-number.java
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            System.out.println(ob.smithNum(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static int sumdig(int n){
+
+        int sum = 0;
+
+        while(n > 0){
+
+            sum += (n%10);
+
+            n /= 10;
+
+        }
+
+        return sum;
+
+    }
+
+    static boolean isPrime(int n){
+
+        for(int i=2; i<=n/2; i++){
+
+            if(n%i == 0){
+
+                return false;
+
+            }
+
+        }
+
+        return true;
+
+    }
+
+    static int smithNum(int n) {
+
+        // code here
+
+        if(isPrime(n)){
+
+            return 0;
+
+        }
+
+        
+
+        int sum = 0;
+
+        int temp = n;
+
+      
+
+        
+
+        int num = 2;
+
+        
+
+        while(temp != 1){
+
+            if(temp%num == 0){
+
+                sum += sumdig(num);
+
+                temp /= num;
+
+            }
+
+            else{
+
+                num++;
+
+            }
+
+        }
+
+        
+
+        if(sum == sumdig(n)){
+
+            return 1;
+
+        }
+
+        else {
+
+            return 0;
+
+        }
+
+    }
+
+
+};
