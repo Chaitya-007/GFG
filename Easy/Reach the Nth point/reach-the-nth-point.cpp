a8db0c4f31bebe3425e64885171fca29bdcd03ce
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2023-12-15 21:43:04 +0530

    Updated Solution - GfG to GitHub

--- Easy/Reach the Nth point/reach-the-nth-point.cpp
+++ Easy/Reach the Nth point/reach-the-nth-point.cpp
@@ -7,7 +7,7 @@ class Solution{
 	public:
 		int nthPoint(int n){
 		    // Code here
-		     int n1=1,n2=2;
+		    int n1=1,n2=2;
             int sum=0;
             if(n==1){
                 return 1;
