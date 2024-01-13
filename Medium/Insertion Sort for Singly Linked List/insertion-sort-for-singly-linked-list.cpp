7b019a677f4c56aba9dfb50c61283eb79b066556
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-01-13 19:01:49 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Insertion Sort for Singly Linked List/insertion-sort-for-singly-linked-list.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Link list node
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};*/
+
+class Solution
+{
+    public:
+    Node* insertionSort(struct Node* head_ref)
+    {
+        //code here
+        if(head_ref==NULL ||head_ref->next==NULL )
+        return head_ref;
+        Node *head=head_ref;
+        Node *cur=head_ref;
+        while(cur!=NULL)
+        {
+            while(head!=cur)
+            {
+                if(head->data>cur->data)
+                {
+                    int d=head->data;
+                    head->data=cur->data;
+                    cur->data=d;
+                }
+                head=head->next;
+            }
+            head=head_ref;
+            cur=cur->next;
+        }
+        return head_ref;
+    }
+    
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int n;
+		cin >> n;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < n; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+        Solution ob;
+
+		head = ob.insertionSort(head);
+		printList(head);
+
+		cout << "\n";
+
+
+
+	}
+	return 0;
+}
+// } Driver Code Ends
