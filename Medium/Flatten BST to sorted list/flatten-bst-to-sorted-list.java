b24a948c801395629b48426bf0d3a964d2ec10c3
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-02-16 10:10:01 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Flatten BST to sorted list/flatten-bst-to-sorted-list.java
@@ -0,0 +1,99 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        data = val;
+        left = right = null;
+    }
+}
+
+public class GFG
+{
+    public static void printList(Node head) {
+        while (head != null) {
+            if (head.left != null)
+                System.out.print("-1 ");
+            System.out.print(head.data + " ");
+            head = head.right;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume newline
+        while (t-- > 0) {
+            String str = sc.nextLine();
+            Node root = buildTree(str);
+            Solution ob = new Solution();
+            Node ans = ob.flattenBST(root);
+            printList(ans);
+        }
+        sc.close();
+    }
+
+    private static Node buildTree(String str) {
+        if (str.length() == 0 || str.charAt(0) == 'N')
+            return null;
+
+        String[] ip = str.split("\\s+");
+        int index = 0;
+
+        Node root = new Node(Integer.parseInt(ip[index++]));
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty() && index < ip.length) {
+            Node currNode = queue.poll();
+
+            String currVal = ip[index++];
+            if (!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+
+            if (index >= ip.length)
+                break;
+            currVal = ip[index++];
+            if (!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+        }
+
+        return root;
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    public void find(ArrayList<Integer> list,Node root){
+        if(root == null)return;
+        find(list,root.left);
+        list.add(root.data);
+        find(list,root.right);
+    }
+    public void convert(Node root,int val){
+        if(root == null)return;
+        if(root.right != null)convert(root.right,val);
+        else root.right = new Node(val);
+    }
+    public Node flattenBST(Node root) {
+        // Code here
+        ArrayList<Integer> list = new ArrayList<>();
+        find(list,root);
+        Node root1 = new Node(list.get(0));
+        for(int i = 1; i < list.size() ; i++){
+            convert(root1,list.get(i));
+        }
+        return root1;
+    }
+}
