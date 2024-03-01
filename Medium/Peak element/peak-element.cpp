6e9af3f5a9a3602c8fc76661423137d8204c1394
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-03-01 20:18:46 +0530

    Updated Solution - GfG to GitHub

--- Medium/Peak element/peak-element.cpp
+++ Medium/Peak element/peak-element.cpp
@@ -11,56 +11,34 @@ using namespace std;
 // n: size of array
 class Solution
 {
-    public:
+public:
     int peakElement(int arr[], int n)
     {
-       // Your code here
-       if(n == 1)
-       {
-           return 0;
-       }
        
-       else if(n == 2)
-       {
-           if(arr[0] >= arr[1])
-           {
-               return 0;
-           }
-           else
-           {
-               return 1;
-           }
-       }
-       
-       else
-       {
-           for(int i = 0; i < n; i++)
-           {
-               if(i == 0)
-               {
-                   if(arr[i] >= arr[i+1])
-                   {
-                       return i;
-                   }
-               }
-               
-               else if(i != (n-1))
-               {
-                   if(arr[i-1] <= arr[i] && arr[i]>= arr[i+1])
-                   {
-                       return i;
-                   }
-               }
-               
-               else
-               {
-                   if(arr[i-1] <= arr[i])
-                   {
-                       return i;
-                   }
-               }
-           }
-       }
+        if (n == 1)
+            return 0;
+
+        for (int i = 0; i < n; i++)
+        {      
+            if (i == 0)
+            {
+                if (arr[i] >= arr[i + 1])
+                    return i;
+            }
+            else if (i == n - 1)
+            {
+                if (arr[i] >= arr[i - 1])
+                    return i;
+            }
+            else
+            {
+                if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+                    return i;
+            }
+        }
+
+        // If no peak element is found, return -1.
+        return -1;
     }
 };
 
