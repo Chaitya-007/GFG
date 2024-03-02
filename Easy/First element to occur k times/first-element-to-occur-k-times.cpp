8ddbcc25ec808b5477a43d4862353d964f20bbe0
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-03-02 22:08:54 +0530

    Updated Solution - GfG to GitHub

--- Easy/First element to occur k times/first-element-to-occur-k-times.cpp
+++ Easy/First element to occur k times/first-element-to-occur-k-times.cpp
@@ -8,17 +8,13 @@ class Solution{
     public:
     int firstElementKTime(int n, int k, int a[])
     {
-       unordered_map<int,int>hashset;
-       for(int i = 0; i < n; i++)
-       {
-           map<int,int>mp;
+        map<int,int>mp;
         for(int i=0;i<n;i++)
         {
             mp[a[i]]++;
             if(mp[a[i]]==k) return a[i];
         }
         return -1;
-       }
     }
 };
 
