ae97615d8d7e1d8511a0ceea9747ff44b578b01e
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-03-03 20:03:20 +0530

    Updated Solution - GfG to GitHub

--- Medium/Largest Number formed from an Array/largest-number-formed-from-an-array.cpp
+++ Medium/Largest Number formed from an Array/largest-number-formed-from-an-array.cpp
@@ -11,22 +11,20 @@ public:
 	// The main function that returns the arrangement with the largest value as
 	// string.
 	// The function accepts a vector of strings
-	
 	static bool comp(string x, string y)
     {
         return x + y > y + x;
     }
-	
-	
-	string printLargest(int n, vector<string> &arr) {
-	    // code here
-	     sort(arr.begin(), arr.end(), comp);
+    
+    string printLargest(int n, vector<string> &arr) {
+        
+        sort(arr.begin(), arr.end(), comp);
         string ans = "";
         
         for(int i = 0;i< n; i++) ans += arr[i];
         
         return ans;
-	}
+    }
 };
 
 //{ Driver Code Starts.
