988c85e832040863d5e307a56e396ee61ba7b838
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-03-13 20:01:17 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Print matrix in diagonal pattern/print-matrix-in-diagonal-pattern.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this method */
+
+class Solution{
+  public:
+    vector<int> matrixDiagonally(vector<vector<int>>&mat)
+    {
+         //Your code here
+         vector<int>v1;
+         int n=mat.size();
+         n=n-1;
+         for(int i=0;i<=2*n;i++){
+             for(int j=0;j<=i;j++){
+                 if(i-j<=n && j<=n){
+                     if(i%2==0){
+                         v1.push_back(mat[i-j][j]);
+                     }
+                     else{
+                         v1.push_back(mat[j][i-j]);
+                     }
+                 }
+             }
+         }
+         return v1;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int T;
+    cin>>T;
+  
+    while(T--)
+    {
+        int n;
+        int k=0;
+        //cin>>k;
+        cin>>n;
+        vector<vector<int>>mat(n, vector<int>(n));
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                cin >> mat[i][j];
+            }
+        }
+        Solution obj;
+        vector<int>ans = obj.matrixDiagonally(mat);
+        for(auto i: ans)cout << i << " ";
+        cout << "\n";
+        
+       
+    }
+}
+// } Driver Code Ends
