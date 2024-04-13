e1fa66845896dc81ba10c8d162813ee2337bca44
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-04-13 11:58:10 +0530

    Updated Solution - GfG to GitHub

--- Easy/Reverse Bits/reverse-bits.cpp
+++ Easy/Reverse Bits/reverse-bits.cpp
@@ -7,31 +7,14 @@ class Solution {
   public:
     long long reversedBits(long long x) {
         // code here
-        vector<int> vec(32,0);
-        for(int i = 0; i < 32; i++)
-        {
-            if(x & 1<<i)
-            {
-                vec[i] = 1;
-            }
-        }
-        
-        // for(auto itr : vec)
-        // {
-        //     cout<<itr;
-        // }
-        // cout<<endl;
-        
         long long sum = 0;
-        
         for(int i = 0; i < 32; i++)
         {
-            if(vec[i] == 1)
+            if(x & (1<<i))
             {
                 sum += (1LL<<(31-i));
             }
         }
-        
         return sum;
     }
 };
