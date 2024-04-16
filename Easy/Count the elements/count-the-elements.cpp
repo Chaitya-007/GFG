77ef46fb17c8e7a5e52ade450246fe26821a0df1
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-04-16 20:11:23 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count the elements/count-the-elements.cpp
+++ Easy/Count the elements/count-the-elements.cpp
@@ -9,30 +9,37 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
+  
     vector<int> countElements(vector<int> &a, vector<int> &b, int n, vector<int> &query,
                               int q) {
         // Your code goes here;
-       
-        
-        
-         vector<int> v;
-       sort(b.begin(), b.end());
-       for(int i = 0; i < q; i++) {
-           int index = query[i];
-           int target = a[index];
-           int l = 0;
-           int r = n - 1;
-           while(l <= r) {
-               int mid = (l + r) / 2;
-               if(b[mid] <= target) {
-                   l = mid + 1;
-               } else {
-                   r = mid - 1;
-               }
-           }
-           v.push_back(l);
-       }
-       return v;
+          sort(b.begin(),b.end());
+          vector<int> result;
+          
+          for(int i = 0; i < q; i++)
+          {
+              int index = query[i];
+              int target = a[index];
+              int l = 0;
+              int r = n - 1;
+              while(l<=r)
+              {
+                  int mid = (l + r)/2;
+                  if(b[mid] <= target)
+                  {
+                      l = mid + 1;
+                  }
+                  else
+                  {
+                      r = mid - 1;
+                  }
+              }
+              
+              result.emplace_back(l);
+          }
+          
+          return result;
+          
     }
 };
 
