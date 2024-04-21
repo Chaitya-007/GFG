42572ab1bd4b8c5cf3a0dc082aa273800efb4238
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-04-21 20:34:18 +0530

    Updated Solution - GfG to GitHub

--- Easy/Three way partitioning/three-way-partitioning.cpp
+++ Easy/Three way partitioning/three-way-partitioning.cpp
@@ -15,20 +15,7 @@ public:
     void threeWayPartition(vector<int>& array,int a, int b)
     {
         // code here 
-         int n = array.size();
-        int low = 0, mid = 0, high = n - 1;
-        while (mid <= high) {
-            if (array[mid] < a) {
-                swap(array[low], array[mid]);
-                low++;
-                mid++;
-            } else if (array[mid] > b) {
-                swap(array[mid], array[high]);
-                high--;
-            } else {
-                mid++;
-            }
-        }
+         sort(array.begin(),array.end());
     }
 };
 
