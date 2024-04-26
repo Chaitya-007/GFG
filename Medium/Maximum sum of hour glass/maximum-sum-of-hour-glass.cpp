15f33cad6bb409a1d4bef6e21dfeb816b8e9d029
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-04-27 00:10:11 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum sum of hour glass/maximum-sum-of-hour-glass.cpp
+++ Medium/Maximum sum of hour glass/maximum-sum-of-hour-glass.cpp
@@ -11,18 +11,22 @@ class Solution {
   public:
     int findMaxSum(int n, int m, vector<vector<int>> mat) {
         // code here
-         if (n < 3 || m < 3)
-        return -1;
-    int ans = -1;
-    for (int i = 0; i < n - 2; i++)
-    {
-        for (int j = 0; j < m - 2; j++)
-        {
-            int maxi = mat[i][j] + mat[i][j + 1] + mat[i][j + 2] + mat[i + 1][j + 1] + mat[i + 2][j] + mat[i + 2][j + 1] + mat[i + 2][j + 2];
-            ans = max(ans, maxi);
-        }
-    }
-    return ans;
+       if(n < 3 || m < 3)
+       {
+           return -1;
+       }
+       
+       int ans = -1;
+       for(int i = 0; i <= n - 3; i++)
+       {
+           for(int j = 0; j <= m - 3; j++)
+           {
+               int sum = mat[i][j] + mat[i][j+1] + mat[i][j+2] + mat[i+1][j+1] + mat[i+2][j] + mat[i+2][j+1] + mat[i+2][j+2];
+               ans = max(ans,sum);
+           }
+       }
+       
+       return ans;
     }
 };
 
