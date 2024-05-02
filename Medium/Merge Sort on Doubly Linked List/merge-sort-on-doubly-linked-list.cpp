7ac2f7b25b0b8564adbd5865744de1fa0059e273
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-05-02 23:29:25 +0530

    Updated Solution - GfG to GitHub

--- Medium/Merge Sort on Doubly Linked List/merge-sort-on-doubly-linked-list.cpp
+++ Medium/Merge Sort on Doubly Linked List/merge-sort-on-doubly-linked-list.cpp
@@ -28,84 +28,143 @@ struct Node
 };
 */
 
+// void merge(vector<int> &vec, int left, int mid, int high)
+// {
+//     int left = low;
+//     int right = mid + 1;
+//     vector<int> temp;
+    
+//     while(left <= mid && right <= high)
+//     {
+//         if(vec[left] <= vec[right])
+//         {
+//             temp.emplace_back(vec[left]);
+//             left++;
+//         }
+//         else
+//         {
+//             temp.emplace_back(vec[right]);
+//             right++;
+//         }
+//     }
+    
+//     while(left <= mid)
+//     {
+//         temp.emplace_back(vec[left]);
+//         left++;
+//     }
+    
+//     while(right <= high)
+//     {
+//         temp.emplace_back(vec[right]);
+//         right++;
+//     }
+    
+//     for(int i = low ; i <= high; i++)
+//     {
+//         vec[i] = temp[i - low];
+//     }
+// }
+
+// void mergesort(vector<int> &vec, int left, int high)
+// {
+//     if(low >= high)
+//     {
+//         return;
+//     }
+    
+//     int mid = (low + high)/2;
+//     mergesort(vec,left,mid);
+//     mergesort(vec,mid+1,high);
+//     merge(vec,left,mid,high);
+// }
+
+// class Solution {
+//   public:
+//     // Function to sort the given doubly linked list using Merge Sort.
+//     struct Node *sortDoubly(struct Node *head) {
+//         // Your code here
+//         vector<int> vec;
+        
+//         struct Node *p = head;
+        
+//         while(p != nullptr)
+//         {
+//             vec.emplace_back(p->data);
+//             p = p->next;
+//         }
+        
+//         int n = vec.size();
+        
+//         void mergesort(vec,0,n-1);
+//     }
+// };
+
 class Solution {
   public:
-    struct Node* merge(struct Node *left, struct Node *right){
-        struct Node *ans = NULL;
-        if(left->data < right->data){
-            ans = left;
-            left = left->next;
-        }else{
-            ans = right;
-            right = right->next;
-        }
-        
-        struct Node *tail = ans;
-        while(left != NULL && right != NULL){
-            if(left->data < right->data){
-                struct Node *x = left;
-                tail->next = x;
-                x->prev = tail;
-                tail = tail->next;
-                left = left->next;
-            }else{
-                struct Node *x = right;
-                tail->next = x;
-                x->prev = tail;
-                tail = tail->next;
-                right = right->next;
-            }
-        }
-        while(left != NULL){
-            struct Node *x = left;
-            tail->next = x;
-            x->prev = tail;
-            tail = tail->next;
-            left = left->next;
-        }
-        while(right != NULL){
-            struct Node *x = right;
-            tail->next = x;
-            x->prev = tail;
-            tail = tail->next;
-            right = right->next;
-        }
-        
-        tail->next = NULL;
-        return ans;
-    }
   
-    struct Node* mergesort(struct Node* head, int n){
-        if(n <= 1)
-            return head;
+  struct Node* merge(struct Node* first, struct Node* second)
+  {
+      if(!first)
+      {
+          return second;
+      }
+      
+      if(!second)
+      {
+          return first;
+      }
+      
+      if(first->data < second->data)
+      {
+          first->next = merge(first->next,second);
+          first->next->prev = first;
+          first->prev = nullptr;
+          
+          return first;
+      }
+      
+      else
+      {
+          second->next = merge(first,second->next);
+          second->next->prev = second;
+          second->prev = nullptr;
+          
+          return second;
+      }
+  }
+   
+    struct Node* getMid(struct Node* head)
+    {
+        struct Node* fast = head;
+        struct Node* slow = head;
         
-        int mid = (n-1)/2;
-        int curr = 0;
-        struct Node *temp1 = head;
-        while(curr < mid){
-            temp1 = temp1->next;
-            curr++;
+        while(fast->next && fast->next->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
         }
         
-        struct Node *temp2 = temp1->next;
-        temp1->next = NULL;
-        temp2->prev = NULL;
+        // will return the mid of next split
+        struct Node* temp = slow->next;
+        slow->next = nullptr;
         
-        struct Node *left = mergesort(head, mid+1);
-        struct Node *right = mergesort(temp2, n-mid-1);
+        return temp;
         
-        return merge(left, right);
     }
-    
     // Function to sort the given doubly linked list using Merge Sort.
     struct Node *sortDoubly(struct Node *head) {
-        int n = 0;
-        struct Node *temp = head;
-        while(temp != NULL){
-            n++;
-            temp = temp->next;
+        
+        if(!head || !head->next)
+        {
+            return head;
         }
-        return mergesort(head, n);
+        
+        struct Node* mid = getMid(head);
+        head = sortDoubly(head);
+        mid = sortDoubly(mid);
+        
+        return merge(head,mid);
     }
 };
 
