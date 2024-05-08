cbd2aa7c1e6eac29ebf7f592ba7375bc4dbeda72
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-05-08 13:07:48 +0530

    Updated Solution - GfG to GitHub

--- Medium/Construct Binary Tree from Inorder and Postorder/construct-binary-tree-from-inorder-and-postorder.cpp
+++ Medium/Construct Binary Tree from Inorder and Postorder/construct-binary-tree-from-inorder-and-postorder.cpp
@@ -49,34 +49,44 @@ struct Node
 class Solution
 {
     public:
+    
+    Node* solve(int in[],int post[], int instart, int inend, int poststart, int postend)
+    {
+        if(instart>inend)
+        {
+            return nullptr;
+        }
+        
+        if(instart == inend)
+        {
+            return new Node(in[instart]);
+        }
+        
+        Node* root = new Node(post[postend]);
+        int index = -1;
+        
+        for(int i = instart; i <= inend; i++)
+        {
+            if(in[i] == post[postend])
+            {
+                index = i;
+                break;
+            }
+        }
+        
+        int left = index - instart;
+        int right = inend - index;
+        
+        root->left = solve(in, post, instart, index - 1, poststart, poststart + left - 1);
+        root->right = solve(in, post, index + 1, inend, postend - right, postend - 1);
+        
+        return root;
+    }
 
     //Function to return a tree created from postorder and inoreder traversals.
     Node *buildTree(int in[], int post[], int n) {
         // Your code here
-         if (n == 0) {
-            return nullptr;
-        }
-
-        // Find the root node index in inorder traversal
-        int rootIndex = 0;
-        while (rootIndex < n && in[rootIndex] != post[n - 1]) {
-            rootIndex++;
-        }
-
-        // Create the root node with the value from postorder traversal
-        Node* root = new Node(post[n - 1]);
-
-        // Recursively build left subtree using elements before root in inorder traversal
-        Node* leftSubtree = buildTree(in, post, rootIndex);
-
-        // Recursively build right subtree using elements after root in inorder traversal
-        Node* rightSubtree = buildTree(in + rootIndex + 1, post + rootIndex, n - rootIndex - 1);
-
-        // Connect left and right subtrees to the root node
-        root->left = leftSubtree;
-        root->right = rightSubtree;
-
-        return root;
+        return solve(in, post, 0, n-1, 0, n-1);
     }
 };
 
