5888f4af00e4f125a33290160bcb09e078024887
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-05-12 15:29:44 +0530

    Updated Solution - GfG to GitHub

--- Medium/Vertical sum/vertical-sum.cpp
+++ Medium/Vertical sum/vertical-sum.cpp
@@ -92,37 +92,33 @@ Node *left,*right;
 */
 class Solution{
   public:
+  
+  map<int, int> mplevel;
+  
+  void preorder(Node* node, int level)
+  {
+      if(node == nullptr)
+      {
+          return;
+      }
+      
+      mplevel[level] += node->data;
+      preorder(node->left, level - 1);
+      preorder(node->right, level + 1);
+  }
+  
     vector <int> verticalSum(Node *root) {
         // add code here.
-          // add code here.
-        map<int, vector<int>>mp;
-        queue<pair<Node*, int>>q;
-        q.push({root, 0});
-
-
-        //BFS 
-        while(!q.empty()){
-            auto x= q.front(); q.pop();
-            Node* r= x.first;
-            int level= x.second;
-            mp[level].push_back(r->data);
-            
-            if(r->left){
-                q.push({r->left, level-1});
-            }
-            if(r->right){
-                q.push({r->right, level+1});
-            }
+        preorder(root,0);
+        vector<int> vec;
+        
+        for(auto itr : mplevel)
+        {
+            vec.emplace_back(itr.second);
         }
-
-//Calculate sum and store in resultant vector.
-        vector<int> res;
-        for(auto x: mp){
-            vector<int> t= x.second;
-           int sum= accumulate(t.begin(), t.end(),0);
-           res.push_back(sum);
-        }
-        return res;
+        
+        return vec;
+        
     }
 };
 
