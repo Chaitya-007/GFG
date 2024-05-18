ebe13b0bcad9d21d2bf6f87a2c6511e3ab95f3d7
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-05-18 15:23:37 +0530

    Updated Solution - GfG to GitHub

--- Easy/Second Largest/second-largest.cpp
+++ Easy/Second Largest/second-largest.cpp
@@ -11,30 +11,21 @@ public:
 	// largest elements
 	int print2largest(int arr[], int n) {
 	    // code here
-	    
-	 int max1 = arr[0];
-	 int max2 = -1;
-	 
-	 for(int i = 0 ; i <  n; i++)
-	 {
-	     if(max1<arr[i])
-	     {
-	         max1 = arr[i];
-	     }
-	 }
-	 
-	 for(int i = 0 ; i < n ; i++)
-	 {
-	     if(arr[i]!= max1 && max2<arr[i])
-	     {
-	         max2 = arr[i];
-	     }
-	 }
-	 
-	 return max2;
+	   // M-1
+	   sort(arr,arr+n);
+	   int slargest = -1;
+	   for(int i = n - 2; i >= 0; i--)
+	   {
+	       if(arr[i] != arr[n-1])
+	       {
+	           slargest = arr[i];
+	           break;
+	       }
+	   }
+	   
+	   return slargest;
 	}
 };
-
 
 //{ Driver Code Starts.
 
