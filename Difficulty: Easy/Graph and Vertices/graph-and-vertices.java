3a613ae975f1ac984a21b5eb44c4ec87e65a90e1
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-06 10:34:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Graph and Vertices/graph-and-vertices.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+    int T = sc.nextInt();
+    while (T-- > 0) {
+      int n = sc.nextInt();
+     
+      Solution obj = new Solution();
+      long ans = obj.count(n);
+      System.out.println(ans);
+    }
+  }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    static long count(int n) {
+    // code here
+    double res = ((n)*(n-1))/2;
+    
+    double val = Math.pow(2,res);
+    return (long)val;
+
+  }
+}
