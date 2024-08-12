8dc36850550d5dd1a47dda7e9a12127266a8f334
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-12 13:38:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -9,125 +9,46 @@ using namespace std;
 
 class Solution{   
 public:
-
-// using dp
-    bool fun(int ind, int target, vector<int> arr)
+    
+    bool fun(int ind, int sum, vector<int> arr)
     {
-        if(target == 0) return true;
+        if(sum == 0)  return true;
         
-        if(ind == 0) return (arr[0] == target);
+        if(ind == 0)  return arr[0] == sum;
         
-        bool notTake = fun(ind - 1, target,arr);
+        bool notTake = fun(ind - 1, sum, arr);
+        
         bool take = false;
         
-        if(arr[ind] <= target)
-        {
-            take = fun(ind - 1, target - arr[ind],arr);
-        }
+        if(arr[ind] <= sum) take = fun(ind-1, sum - arr[ind], arr);
         
-        return (take | notTake);
+        return notTake | take;
     }
     
-    // using dp
-    bool funDP(int ind, int target, vector<int> arr, vector<vector<int>>& dp)
+     bool fundp(int ind, int sum, vector<int> arr,  vector<vector<int>> &dp)
     {
-        if(target == 0) return true;
+        if(sum == 0)  return true;
         
-        if(ind == 0) return (arr[0] == target);
+        if(ind == 0)  return arr[0] == sum;
         
-        if(dp[ind][target] != -1) return dp[ind][target];
+        if(dp[ind][sum] != -1)  return dp[ind][sum];
         
-        bool notTake = funDP(ind - 1, target,arr,dp);
+        bool notTake = fundp(ind - 1, sum, arr, dp);
         
         bool take = false;
         
-        if(arr[ind] <= target)
-        {
-            take = funDP(ind - 1, target - arr[ind],arr,dp);
-        }
+        if(arr[ind] <= sum) take = fundp(ind-1, sum - arr[ind], arr, dp);
         
-        return dp[ind][target] = (take | notTake);
+        return dp[ind][sum] = notTake | take;
     }
 
     bool isSubsetSum(vector<int>arr, int sum){
         // code here 
         int n = arr.size();
-        
-        // Using recursion
         // return fun(n-1,sum,arr);
-        // T.C -> 2^n
-        // S.C -> O(N)
         
-        // Using dp
-        // vector<vector<int>> dp(n,vector<int> (sum + 1, -1));
-        // return funDP(n-1,sum,arr,dp);
-        // T.C -> O(N*target)
-        // S.C -> O(N) + O(M*N)
-        
-        // Tabulation
-        // T.C -> O(N*target)
-        // S.C -> O(N * target)
-        
-    
-        // vector<vector<bool>> dp(n, vector<bool>(sum + 1, 0));
-        
-        // for(int i = 0; i < n; i++)
-        // {
-        //     dp[i][0] = true;
-        // }
-        
-        // dp[0][arr[0]] = true;
-        
-        // for(int ind = 1; ind < n; ind++)
-        // {
-        //     for(int target = 1; target <= sum; target++)
-        //     {
-        //         bool notTake = dp[ind - 1][target];
-        //         bool take = false;
-        //         if(arr[ind] <= target)
-        //         {
-        //             take = dp[ind - 1][target - arr[ind]];
-        //         }
-                
-        //         dp[ind][target] = (take | notTake);
-        //     }
-        // }
-        
-        
-        // return dp[n-1][sum];
-        
-        vector<bool> prev(sum+1,0);
-        vector<bool> curr(sum+1,0);
-        
-        prev[0] = curr[0] = true;
-        
-        if(arr[0] <= sum)
-        {
-            
-        prev[arr[0]] = true;
-        }
-        
-        for(int ind = 1; ind < n; ind++)
-        {
-            for(int target = 1; target <= sum; target++)
-            {
-                bool notTake = prev[target];
-                bool take = false;
-                if(arr[ind] <= target)
-                {
-                    take = prev[target - arr[ind]];
-                }
-                
-                curr[target] = (take | notTake);
-            }
-            
-            prev = curr;
-        }
-        
-        return prev[sum];
-        
-        
-        
+        vector<vector<int>> dp(n,vector<int> (sum + 1, -1));
+        return fundp(n-1,sum,arr,dp);
     }
 };
 
