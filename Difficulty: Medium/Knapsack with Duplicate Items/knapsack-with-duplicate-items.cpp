3ab2d01a29c559fa0b8d663ef0206191c92a93eb
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-19 13:58:10 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+
+    int fun(int ind, int W, int val[], int wt[])
+    {
+        
+        if(ind == 0)
+        {
+            if(wt[0] <= W)
+            {
+                return (W/wt[0]) * val[0];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
+        int notTake = fun(ind - 1, W, val, wt);
+        int take = INT_MIN;
+        
+        if(wt[ind] <= W)
+        {
+            take = val[ind] + fun(ind, W - wt[ind], val, wt);
+        }
+        
+        return max(take, notTake);
+    }
+    
+    int fundp(int ind, int W, int val[], int wt[], vector<vector<int>> &dp)
+    {
+        
+        if(ind == 0)
+        {
+            if(wt[0] <= W)
+            {
+                return (W/wt[0]) * val[0];
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        
+        if(dp[ind][W] != -1) return dp[ind][W];
+        
+        int notTake = fundp(ind - 1, W, val, wt, dp);
+        int take = INT_MIN;
+        
+        if(wt[ind] <= W)
+        {
+            take = val[ind] + fundp(ind, W - wt[ind], val, wt, dp);
+        }
+        
+        return dp[ind][W] = max(take, notTake);
+    }
+
+    int knapSack(int N, int W, int val[], int wt[])
+    {
+        // code here
+        // return fun(N-1,W,val,wt);
+        vector<vector<int>> dp(N, vector<int> (W+1,-1));
+        return fundp(N-1, W, val, wt, dp);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int N, W;
+        cin>>N>>W;
+        int val[N], wt[N];
+        for(int i = 0;i < N;i++)
+            cin>>val[i];
+        for(int i = 0;i < N;i++)
+            cin>>wt[i];
+        
+        Solution ob;
+        cout<<ob.knapSack(N, W, val, wt)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
