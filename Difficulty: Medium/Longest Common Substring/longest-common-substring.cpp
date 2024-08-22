4a07588f9a0aaa5471a5d947d8b735eb7a1cf89c
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-22 17:38:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -6,67 +6,33 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  
-
-  
     int longestCommonSubstr(string str1, string str2) {
         // your code here
-        
-        // don,t write code for shortest common supersequence
         int n = str1.length();
         int m = str2.length();
         
-        // Dont use recursion******************************
-        
-        // use tabulation
-        
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-
-        for(int i = 0; i <= n; i++)  dp[i][0] = 0;
-        for(int j = 0; j <= m; j++)  dp[0][j] = 0;
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1,0));
         int ans = 0;
-
+        
         for(int i = 1; i <= n; i++)
         {
             for(int j = 1; j <= m; j++)
             {
-                if(str1[i-1] == str2[j-1]) 
+                if(str1[i-1] == str2[j-1])
                 {
-                    
-                dp[i][j] = 1 + dp[i-1][j-1];
-                ans = max(ans,dp[i][j]);
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    ans = max(ans,dp[i][j]);
                 }
-        
                 else
-                dp[i][j] = 0;
+                {
+                    dp[i][j] = 0;
+                }
             }
         }
-
+        
         return ans;
-        
-        // vector<int> prev(m+1,0);
-        // vector<int> curr(m+1,0);
-        // int ans = 0;
-        
-        // for(int i = 1; i <= n; i++)
-        // {
-        //     for(int j = 1; j <= m; j++)
-        //     {
-        //         if(str1[i-1] == str2[j-1])
-        //         {
-        //         curr[j] = 1 + prev[j-1];
-        //         ans = max(ans,curr[j]);
-        //         }
-        //         else
-        //         curr[j] = 0;
-        //     }
-        //     prev = curr;
-        // }
-        
-        // return ans;
     }
 };
-
 
 //{ Driver Code Starts.
 
