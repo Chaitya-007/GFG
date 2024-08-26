1be1799c8eeb227257d7dc550f0238040b5ad5c8
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-08-26 21:39:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Wildcard Pattern Matching/wildcard-pattern-matching.cpp
+++ Difficulty: Hard/Wildcard Pattern Matching/wildcard-pattern-matching.cpp
@@ -5,9 +5,7 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-  public:
-    /*You are required to complete this method*/
-    
+    public:
     int fun(int i, int j, string p, string s)
     {
         if(i < 0 && j < 0) return true;
@@ -70,14 +68,49 @@ class Solution {
         return dp[i][j] = false;
     }
     
+     int funDpOneShift(int i, int j, string &p, string &s, vector<vector<int>> &dp)
+    {
+        if(i == 0 && j == 0) return true;
+        if(i == 0 && j > 0) return false;
+        if(i > 0 && j == 0)
+        {
+            for(int m = 1; m <= i; m++)
+            {
+                if(p[m-1] != '*')
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        if(p[i-1] == '*')
+        {
+            return dp[i][j] = funDpOneShift(i-1,j,p,s,dp) | funDpOneShift(i,j-1,p,s,dp);
+        }
+        
+        if(p[i-1] == s[j-1] || p[i-1] == '?')
+        {
+            return dp[i][j] = funDpOneShift(i-1,j-1,p,s,dp);
+        }
+        
+        return dp[i][j] = false;
+    }
+    
     int wildCard(string pattern, string str) {
         // code here
         int n = pattern.length();
         int m = str.length();
         // return fun(n-1,m-1,pattern,str);
         
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return funDp(n-1,m-1,pattern,str,dp);
+        // vector<vector<int>> dp(n, vector<int> (m, -1));
+        // return funDp(n-1,m-1,pattern,str,dp);
+        
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+        return funDpOneShift(n,m,pattern,str,dp);
     }
 };
 
