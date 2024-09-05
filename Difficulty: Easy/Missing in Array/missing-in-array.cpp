f91e3292b8a7483d21520e32d02239baf5dc4199
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-09-05 23:09:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Missing in Array/missing-in-array.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, vector<int>& arr) {
+
+        // Your code goes here
+         int xor1 = 0, xor2 = 0;
+
+    // XOR all array elements
+    for (int i = 0; i < n - 1; i++) {
+        xor2 ^= arr[i];
+    }
+
+    // XOR all numbers from 1 to n
+    for (int i = 1; i <= n; i++) {
+        xor1 ^= i;
+    }
+
+    // Missing number is the XOR of xor1 and xor2
+    return xor1 ^ xor2;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n - 1);
+        for (int i = 0; i < n - 1; ++i)
+            cin >> arr[i];
+        Solution obj;
+        cout << obj.missingNumber(n, arr) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
