8b12ee2d1af4817a3ce26e69d519c2917e840ede
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2024-10-14 21:16:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
+++ Difficulty: Medium/Longest Sub-Array with Sum K/longest-sub-array-with-sum-k.java
@@ -1,61 +1,55 @@
 //{ Driver Code Starts
-//Initial Template for Java
+// Initial Template for Java
 
 /*package whatever //do not write package name here */
 
 import java.io.*;
 import java.util.*;
 
-
 class Array {
-    
+
     // Driver code
-	public static void main (String[] args) throws IOException{
-		// Taking input using buffered reader
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int testcases = Integer.parseInt(br.readLine());
-		
-		// looping through all testcases
-		while(testcases-- > 0){
-		    String line = br.readLine();
-		    String[] element = line.trim().split("\\s+");
-		    int sizeOfArray = Integer.parseInt(element[0]);
-		    int K = Integer.parseInt(element[1]);
-		    
-		    int arr [] = new int[sizeOfArray];
-		    
-		    line = br.readLine();
-		    String[] elements = line.trim().split("\\s+");
-		    for(int i = 0;i<sizeOfArray;i++){
-		        arr[i] = Integer.parseInt(elements[i]);
-		    }
-		    
-		    Solution obj = new Solution();
-		    int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
-		    
-		    System.out.println(res);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+
+        // looping through all testcases
+        while (testcases-- > 0) {
+            String line = br.readLine();
+            String[] element = line.trim().split("\\s+");
+            int sizeOfArray = Integer.parseInt(element[0]);
+            int K = Integer.parseInt(element[1]);
+
+            int arr[] = new int[sizeOfArray];
+
+            line = br.readLine();
+            String[] elements = line.trim().split("\\s+");
+            for (int i = 0; i < sizeOfArray; i++) {
+                arr[i] = Integer.parseInt(elements[i]);
+            }
+
+            Solution obj = new Solution();
+            int res = obj.lenOfLongSubarr(arr, sizeOfArray, K);
+
+            System.out.println(res);
+        }
+    }
 }
-
-
 
 // } Driver Code Ends
 
 
-//User function Template for Java
+// User function Template for Java
 
-class Solution{
-    
-   
+class Solution {
     // Function for finding maximum and value pair
-    public static int lenOfLongSubarr (int A[], int N, int K) {
-        //Complete the function
-        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
-        
+    public static int lenOfLongSubarr(int A[], int N, int K) {
+        // Complete the function
         int sum = 0;
         int maxlen = 0;
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         
         for(int i = 0; i < N; i++)
         {
@@ -63,27 +57,25 @@ class Solution{
             
             if(sum == K)
             {
-                maxlen = Math.max(maxlen,i + 1);
+                int len = i + 1;
+                maxlen = Math.max(len,maxlen);
             }
             
             int rem = sum - K;
-            
-            if(mp.containsKey(rem))
+            if(map.containsKey(rem))
             {
-                int len = i - mp.get(rem);
+                int index = map.get(rem);
+                int len = i - index;
                 maxlen = Math.max(maxlen,len);
             }
             
-            if(!mp.containsKey(sum))
+            
+            if(!map.containsKey(sum))
             {
-                mp.put(sum, i);
+                map.put(sum,i);
             }
         }
         
         return maxlen;
     }
-    
-    
 }
-
-
