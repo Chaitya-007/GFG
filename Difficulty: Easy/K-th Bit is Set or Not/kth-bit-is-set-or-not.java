6022c02279e3348c11aa726123dd9d8b4c018c4e
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-27 15:10:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/K-th Bit is Set or Not/kth-bit-is-set-or-not.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class CheckBit {
+    // Function to check if Kth bit is set or not.
+    static boolean checkKthBit(int n, int k) {
+        // Your code here
+        if((n & (1<<k)) != 0)
+        return true;
+        
+        return false;
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    static int n;
+    static int k;
+
+    // Driver Code
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine()); // Inputting the testcases
+
+        while (t > 0) // While testcases exist
+        {
+
+            n = Integer.parseInt(br.readLine()); // Input N
+            k = Integer.parseInt(br.readLine()); // Input K
+
+            CheckBit obj = new CheckBit();
+
+            if (obj.checkKthBit(n, k))
+                System.out.println("true"); // If true, print Yes
+
+            else
+                System.out.println("false"); // Else print No
+
+            t--;
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
