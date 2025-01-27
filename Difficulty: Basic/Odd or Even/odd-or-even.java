04955ce5ba120173ef1c4a8a91f2da40b3914c12
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-27 15:12:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Odd or Even/odd-or-even.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+
+            Solution ob = new Solution();
+            if (ob.isEven(N))
+                System.out.println("true");
+            else
+                System.out.println("false");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+        if( (n&1) != 0)
+        return false;
+        
+        return true;
+    }
+}
