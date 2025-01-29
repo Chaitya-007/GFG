5161209d681af5e68d3b21668257a02d921703a7
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-01-29 09:35:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Two numbers with odd occurrences/two-numbers-with-odd-occurrences.java
+++ Difficulty: Medium/Two numbers with odd occurrences/two-numbers-with-odd-occurrences.java
@@ -39,31 +39,41 @@ class Solution
     public int[] twoOddNum(int Arr[], int N)
     {
         // code here
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        long xor = 0;
         
         for(int val : Arr)
         {
-            map.put(val, map.getOrDefault(val,0) + 1);
+            xor ^= val;
         }
         
-        int[] arr = new int[2];
-        int ind = 0;
+        int rightMost = ((int)(xor & (xor - 1))) ^ (int)xor;
         
-        for(Map.Entry<Integer, Integer> e : map.entrySet())
+        int b1 = 0;
+        int b2 = 0;
+        
+        for(int val : Arr)
         {
-            if ( (e.getValue() & 1) == 1)
+            if((rightMost & val) != 0)
             {
-                arr[ind] = e.getKey();
-                ind++;
+                b1 ^= val;
+            }
+            else
+            {
+                b2 ^= val;
             }
         }
         
-        // Arrays.sort(arr, Collections.reverseOrder());
-        if(arr[0] < arr[1])
+        int[] arr = new int[2];
+        
+        if(b1 > b2)
         {
-            int temp = arr[0];
-            arr[0] = arr[1];
-            arr[1] = temp;
+            arr[0] = b1;
+            arr[1] = b2;
+        }
+        else
+        {
+            arr[0] = b2;
+            arr[1] = b1;
         }
         
         return arr;
