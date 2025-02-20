e88db4e3611830847b13817a43437f869df04168
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-20 16:29:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Postfix to Infix Conversion/postfix-to-infix-conversion.java
+++ Difficulty: Medium/Postfix to Infix Conversion/postfix-to-infix-conversion.java
@@ -28,15 +28,17 @@ System.out.println("~");
 // User function Template for Java
 
 class Solution {
+    
     static String postToInfix(String exp) {
         // code here
-        Stack<String> st = new Stack<String>();
+        Stack<String> st = new Stack<>();
+        StringBuilder sb = new StringBuilder("");
         
         for(char ch : exp.toCharArray())
         {
-            if ( ('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z' ) || ('0' <= ch && ch <= '9') )
+            if( ('A' <= ch && ch <= 'Z') || ('a' <= ch && ch <= 'z') || ('0' <= ch && ch <= '9') )
             {
-                StringBuilder sb = new StringBuilder("");
+                sb.setLength(0);
                 sb.append(ch);
                 st.push(sb.toString());
             }
@@ -46,14 +48,14 @@ class Solution {
                 st.pop();
                 StringBuilder t2 = new StringBuilder(st.peek());
                 st.pop();
-                StringBuilder res = new StringBuilder("");
-                res.append('(');
-                res.append(t2);
-                res.append(ch);
-                res.append(t1);
-                res.append(')');
+                sb.setLength(0);
+                sb.append('(');
+                sb.append(t2);
+                sb.append(ch);
+                sb.append(t1);
+                sb.append(')');
+                st.push(sb.toString());
                 
-                st.push(res.toString());
             }
         }
         
