bc98f8b8da327eb760b7c4dcb612576dc829b4b6
Chaitanya Asole <120618336+Chaitya-007@users.noreply.github.com>
2025-02-27 16:41:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.java
+++ Difficulty: Medium/Job Sequencing Problem/job-sequencing-problem.java
@@ -5,77 +5,125 @@ import java.util.*;
 
 class Job {
     int id, profit, deadline;
-    Job(int x, int y, int z){
+
+    Job(int x, int y, int z) {
         this.id = x;
         this.deadline = y;
-        this.profit = z; 
+        this.profit = z;
     }
 }
 
 class GfG {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        //testcases
-		int t = Integer.parseInt(br.readLine().trim());
-		while(t-->0){
-            String inputLine[] = br.readLine().trim().split(" ");
-            
-            //size of array
-            int n = Integer.parseInt(inputLine[0]);
-            Job[] arr = new Job[n];
-            inputLine = br.readLine().trim().split(" ");
-            
-            //adding id, deadline, profit
-            for(int i=0, k=0; i<n; i++){
-                arr[i] = new Job(Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]), Integer.parseInt(inputLine[k++]));
+
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            String[] jobIDStrings = sc.nextLine().split(" ");
+            String[] deadlineStrings = sc.nextLine().split(" ");
+            String[] profitStrings = sc.nextLine().split(" ");
+
+            // Convert the input strings to integer arrays
+            int[] jobIDs = new int[jobIDStrings.length];
+            int[] deadlines = new int[deadlineStrings.length];
+            int[] profits = new int[profitStrings.length];
+
+            for (int i = 0; i < jobIDStrings.length; i++) {
+                jobIDs[i] = Integer.parseInt(jobIDStrings[i]);
+                deadlines[i] = Integer.parseInt(deadlineStrings[i]);
+                profits[i] = Integer.parseInt(profitStrings[i]);
             }
-            
-            Solution ob = new Solution();
-            
-            //function call
-            int[] res = ob.JobScheduling(arr, n);
-            System.out.println (res[0] + " " + res[1]);
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.JobSequencing(jobIDs, deadlines, profits);
+            System.out.println(ans.get(0) + " " + ans.get(1));
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
 
-class Solution
+class Pair<K, V>
 {
-    //Function to find the maximum profit and the number of jobs done.
-    int[] JobScheduling(Job arr[], int n)
+    private K key;
+    private V value;
+    
+    Pair(K key, V value)
     {
-        // Your code here
-          // Your code here
-        Arrays.sort(arr,(x,y)->y.profit-x.profit);
-        int maxDeadline=0;
-        for(Job j:arr) maxDeadline=Math.max(maxDeadline,j.deadline);
-        int deadlines[]=new int[maxDeadline+1];
-        Arrays.fill(deadlines,-1);
-        int maxProfit=0,totalJobs=0;
-        for(Job j:arr){
-            for(int i=j.deadline;i>0;i--){
-                if(deadlines[i]==-1){
-                    deadlines[i]=j.profit;
-                    maxProfit+=j.profit;
-                    totalJobs++;
-                    break;
-                }
-            }
-        }
-        return new int[]{totalJobs,maxProfit};
+        this.key = key;
+        this.value = value;
+    }
+    
+    public K getKey()
+    {
+        return key;
+    }
+    
+    public V getValue()
+    {
+        return value;
     }
 }
 
-/*
-class Job {
-    int id, profit, deadline;
-    Job(int x, int y, int z){
-        this.id = x;
-        this.deadline = y;
-        this.profit = z; 
+
+class Solution {
+
+    public ArrayList<Integer> JobSequencing(int[] id, int[] deadline, int[] profit) {
+        // code here..
+        
+        List<Pair<Integer, Integer> > list = new ArrayList<>();
+        int n = id.length;
+        
+        int maxo = 0;
+        
+        for(int val : deadline)
+        {
+            maxo = Math.max(maxo,val);
+        }
+        
+        int[] res = new int[maxo+1];
+        Arrays.fill(res,0);
+        
+        for(int i = 0; i < n; i++)
+        {
+            list.add(new Pair<>(deadline[i],profit[i]));
+        }
+        
+        Comparator<Pair<Integer, Integer>> comp = (p1,p2) -> 
+        {
+          return Integer.compare(p1.getValue(), p2.getValue());  
+        };
+        
+        Collections.sort(list,comp);
+        
+        int cnt = 0;
+        int sum = 0;
+        
+        for(int i = n - 1; i >= 0; i--)
+        {
+            int pf = list.get(i).getValue();
+            int dd = list.get(i).getKey();
+            
+            while(dd != 0 && res[dd] != 0)
+            {
+                dd--;
+            }
+            
+            if(dd != 0) 
+            {
+                cnt++;
+                res[dd] = pf;
+                sum += pf;
+            }
+        }
+        
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        
+        ans.add(cnt);
+        ans.add(sum);
+        
+        return ans;
+        
     }
 }
-*/
